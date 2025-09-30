fefaaeb1f30e4d3f37b4899b092a0d08e1e4e140
aajlil <aymanajlil6@gmail.com>
2025-09-30 15:05:02 +0200

    Refactoring de la classe Terrain

--- src/main/java/fr/mbouklikha/dev/sae_glacium/modeles/monde/Terrain.java
+++ src/main/java/fr/mbouklikha/dev/sae_glacium/modeles/monde/Terrain.java
@@ -7,45 +7,41 @@ import java.util.ArrayList;
 
 public class Terrain {
     private ArrayList<Hitbox> hitboxBlocsSolides;
-    private final int TAILLE_BLOC = 32;
+    public static final int TAILLE_BLOC = 32;
 
     public Terrain() {
-        hitboxBlocsSolides = new ArrayList<>();
-
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[0].length; x++) {
-                int val = map[y][x];
-                if (val == 1 || val == 2) {
-                    // crée une hitbox pour ce bloc solide
-                    int posX = x * TAILLE_BLOC;
-                    int posY = y * TAILLE_BLOC;
-                    hitboxBlocsSolides.add(new Hitbox(posX, posY, TAILLE_BLOC, TAILLE_BLOC));
-                }
-            }
-        }
+        this.hitboxBlocsSolides = new ArrayList<>();
+        construireHitboxDepuisMap();
     }
 
-
-
-    /*
-     * Met à jour la liste des hitbox des blocs solides en fonction de la carte actuelle.
-     * Cette méthode vide la liste précédente et reconstruit les hitbox à partir de la carte.
-     * Utile si la carte change dynamiquement pendant le jeu.
-    */
-    public void mettreAJourHitboxBlocsSolides() {
+    // Méthode privée factorisée
+    private void construireHitboxDepuisMap() {
         hitboxBlocsSolides.clear();
-        int[][] map = getMap();
-
         for (int y = 0; y < map.length; y++) {
             for (int x = 0; x < map[0].length; x++) {
-                if (map[y][x] == 1 || map[y][x] == 2) { // blocs solides
-                    // Créer une hitbox pour ce bloc
-                    Hitbox hb = new Hitbox(x * TAILLE_BLOC, y * TAILLE_BLOC, TAILLE_BLOC, TAILLE_BLOC);
-                    hitboxBlocsSolides.add(hb);
+                if (map[y][x] == 1 || map[y][x] == 2) {
+                    hitboxBlocsSolides.add(new Hitbox(x * TAILLE_BLOC, y * TAILLE_BLOC, TAILLE_BLOC, TAILLE_BLOC));
                 }
             }
         }
     }
+
+    // Utile si la map change dynamiquement
+    public void mettreAJourHitboxBlocsSolides() {
+        construireHitboxDepuisMap();
+    }
+
+    public int[][] getMap() {
+        return map;
+    }
+
+
+    public ArrayList<Hitbox> getHitboxBlocsSolides() {
+        return hitboxBlocsSolides;
+    }
+
+
+
 
 
 
@@ -53,7 +49,7 @@ public class Terrain {
      * La carte du terrain, représentée par un tableau 2D d'entiers.
      * Chaque entier représente un type de bloc ou un espace vide.
     */
-    public int[][] map = {
+    private int[][] map = {
             {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
             {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
             {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
@@ -76,14 +72,6 @@ public class Terrain {
             {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
     };
 
-
-    public int[][] getMap() {
-        return map;
-    }
-
-    public ArrayList<Hitbox> getHitboxBlocsSolides() {
-        return hitboxBlocsSolides;
-    }
 
 
 }

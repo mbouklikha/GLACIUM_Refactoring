d038c425f481605d072b0b6d2e1da19adb175e67
aajlil <aymanajlil6@gmail.com>
2025-10-07 16:15:43 +0200

    Refactoring

--- src/main/java/fr/mbouklikha/dev/sae_glacium/modeles/objets/Neige.java
+++ src/main/java/fr/mbouklikha/dev/sae_glacium/modeles/objets/Neige.java
@@ -10,35 +10,6 @@ public class Neige extends Ressource {
     }
 
     public void fonction(int x, int y){
-        int[][] map = getTerrain().getMap();
-        Objets objetEnMain = getSid().getObjetEnMain();
-
-        // Vérifie si l'objet en main est encore dans l'inventaire avec une quantité > 0
-        Item itemTrouve = null;
-        for (Item item : getInventaire().getItems()) {
-            if (item.getObjet().equals(objetEnMain)) {
-                itemTrouve = item;
-                break;
-            }
-        }
-
-        if (itemTrouve == null || itemTrouve.getQuantite().get() == 0) {
-            System.out.println("Impossible de poser : objet non disponible ou quantité insuffisante.");
-
-        } else{
-            // Poser le bloc
-            if (map[y][x] == -1) {
-                map[y][x] = 1;
-                getInventaire().retirerUnItem(objetEnMain);
-
-                // Si l’objet a disparu de l’inventaire, on enlève aussi de la main
-                if (!getInventaire().contient(objetEnMain)) {
-                    getSid().setObjetEnMain(null);
-                }
-
-                getTerrain().mettreAJourHitboxBlocsSolides();
-                System.out.println("Bloc posé");
-            }
-        }
+        poserBloc(x, y, 1); // id du bloc de neige
     }
 }
--- src/main/java/fr/mbouklikha/dev/sae_glacium/modeles/objets/Ressource.java
+++ src/main/java/fr/mbouklikha/dev/sae_glacium/modeles/objets/Ressource.java
@@ -20,4 +20,37 @@ public abstract class Ressource extends Objets {
 
     public abstract void fonction(int x, int y);
 
+    protected void poserBloc(int x, int y, int idBloc) {
+        int[][] map = getTerrain().getMap();
+        Objets objetEnMain = getSid().getObjetEnMain();
+
+        // Recherche de l'objet dans l'inventaire
+        Item itemTrouve = null;
+        for (Item item : getInventaire().getItems()) {
+            if (item.getObjet().equals(objetEnMain)) {
+                itemTrouve = item;
+            }
+        }
+
+        if (itemTrouve == null || itemTrouve.getQuantite().get() == 0) {
+            System.out.println("Impossible de poser : objet non disponible ou quantité insuffisante.");
+        } else {
+            if (map[y][x] == -1) {
+                map[y][x] = idBloc;
+                getInventaire().retirerUnItem(objetEnMain);
+
+                // Si l’objet a disparu de l’inventaire, on enlève aussi de la main
+                if (!getInventaire().contient(objetEnMain)) {
+                    getSid().setObjetEnMain(null);
+                }
+
+                getTerrain().mettreAJourHitboxBlocsSolides();
+                System.out.println("Bloc de " + getNom() + " posé");
+            } else {
+                System.out.println("Impossible de poser : case déjà occupée");
+            }
+        }
+    }
+
+
 }

fb0297e27c4bebec5ab5c63497764968f5e8317c
mbouklikha <m.bouklikha@yahoo.com>
2025-10-14 15:52:43 +0200

    Correction dans le controller (terrain)

--- src/main/java/fr/mbouklikha/dev/sae_glacium/controller/Controller.java
+++ src/main/java/fr/mbouklikha/dev/sae_glacium/controller/Controller.java
@@ -116,7 +116,7 @@ public class Controller {
 
     public void initialiserEnvironnementActeurs(){
         this.env = Environnement.getInstance();
-        TerrainVue terrainVue = new TerrainVue(env.getTerrain(), tilePane);
+        TerrainVue terrainVue = new TerrainVue(tilePane);
 
         sid = new Sid(env);
         sidVue = new SidVue(sid, zoneJeu);
